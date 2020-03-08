f368f933ed966bbbf0465af3b38102dec01c52ad
Daniel <daniel.kane7@gmail.com>
2020-03-08 12:49:20 -0400

    added async

--- app/src/main/java/com/example/bopit/Solo.kt
+++ app/src/main/java/com/example/bopit/Solo.kt
@@ -1,13 +1,17 @@
 package com.example.bopit
 
+import android.os.AsyncTask
+import android.os.AsyncTask.execute
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.solo_layout.*
-import kotlin.random.Random
+import java.lang.String
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 
 class Solo : AppCompatActivity() {
@@ -34,49 +38,65 @@ class Solo : AppCompatActivity() {
 
         go.setOnClickListener {
             start_game()
+
         }
-
-
-
-
-
     }
 
     fun start_game() {
-        var i: Int = 0
-        //loop
-        do {
-            startTime = SimpleDateFormat("ss", Locale.getDefault()).format(Date()).toInt()
-            randomNum = Random.nextInt(1, 4)
-
-            //function for displaying the instruction (takes the random number as input)
-
-            displayInstruction(randomNum)
-
+        doAsync{
+            var i: Int = 0
+            //loop
             do {
-                currentTime = SimpleDateFormat("ss", Locale.getDefault()).format(Date()).toInt()
-            } while (currentTime - startTime < interval)
+                startTime = SimpleDateFormat("ss", Locale.getDefault()).format(Date()).toInt()
+                randomNum = Random.nextInt(1, 5)
 
-            Log.d("PlayGame", "timer ran out")
-            Log.d("Random", randomNum.toString())
+                //function for displaying the instruction (takes the random number as input)
 
-            i++
-        } while (i < 5)
+                shakeTxt.post({displayInstruction(randomNum)})
+
+                do {
+                    currentTime = SimpleDateFormat("ss", Locale.getDefault()).format(Date()).toInt()
+                } while (currentTime - startTime < interval)
+
+                Log.d("PlayGame", "timer ran out")
+                Log.d("Random", randomNum.toString())
+
+                i++
+            } while (i < 5)
+        }
 
     }
 
     private fun displayInstruction(rd: Int) {
         when (rd) {
-            1 -> { shakeTxt.text = "Shake it!" }
+            1 -> {
+                shakeTxt.setText("Shake it!")
+            }
 
-            2 -> { shakeTxt.text = "Spin it" }
+            2 -> {
+                shakeTxt.setText("Flip it!")
+            }
 
-            3 -> { shakeTxt.text = "Flip it"}
+            3 -> {
+                shakeTxt.setText("Spin it!")
+            }
 
-            4 -> { shakeTxt.text = "Tap it"}
+            4 -> {
+                shakeTxt.setText("Tap it!")
+            }
         }
+
     }
 
+}
 
+class doAsync(val handler: () -> Unit) : AsyncTask<Void, Void, Void>() {
+    init {
+        execute()
+    }
 
+    override fun doInBackground(vararg params: Void?): Void? {
+        handler()
+        return null
+    }
 }
